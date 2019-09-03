afcb7fa1b8bcd0f59a29188d7c40016097e10160
Scerelyn <Scerelyn@gmail.com>
2019-09-02 19:57:07 -0700

    Add converters and brush theme class

---
+++ Minesweeper/Converters/CellStateToBrushConverter.cs
@@ -0,0 +1,63 @@
+﻿using Minesweeper.Logic;
+using Minesweeper.Resources;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Minesweeper.Converters
+{
+    public class CellStateToBrushConverter : IValueConverter
+    {
+        public BrushTheme ActiveTheme { get; set; } = BrushThemes.Default;
+        public object Convert(object value, Type targetTypem, object parameter, CultureInfo culture)
+        {
+            if (value is CellState state)
+            {
+                switch (state)
+                {
+                    case CellState.Hidden:
+                        return ActiveTheme.Hidden;
+                    case CellState.One:
+                        return ActiveTheme.One;
+                    case CellState.Two:
+                        return ActiveTheme.Two;
+                    case CellState.Three:
+                        return ActiveTheme.Three;
+                    case CellState.Four:
+                        return ActiveTheme.Four;
+                    case CellState.Five:
+                        return ActiveTheme.Five;
+                    case CellState.Six:
+                        return ActiveTheme.Six;
+                    case CellState.Seven:
+                        return ActiveTheme.Seven;
+                    case CellState.Eight:
+                        return ActiveTheme.Eight;
+                    case CellState.Flagged:
+                        return ActiveTheme.Flagged;
+                    case CellState.Mine:
+                        return ActiveTheme.Mine;
+                    case CellState.Exploded:
+                        return ActiveTheme.Exploded;
+                    case CellState.Error:
+                    default:
+                        return ActiveTheme.Error;
+                }
+            }
+            else
+            {
+                return ActiveTheme.Error;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetTypem, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Minesweeper/Resources/MineBrushes.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Minesweeper.Resources
+{
+    /// <summary>
+    /// Represents a set of Brushes for a Theme for Minesweeper
+    /// </summary>
+    public class BrushTheme
+    {
+        /// <summary>
+        /// The Brush for hidden/default cells
+        /// </summary>
+        public Brush Hidden { get; set; }
+        /// <summary>
+        /// The Brush for the cell containing the Number 1
+        /// </summary>
+        public Brush One { get; set; }
+        /// <summary>
+        /// The Brush for the cell containing the Number 2
+        /// </summary>
+        public Brush Two { get; set; }
+        /// <summary>
+        /// The Brush for the cell containing the Number 3
+        /// </summary>
+        public Brush Three { get; set; }
+        /// <summary>
+        /// The Brush for the cell containing the Number 4
+        /// </summary>
+        public Brush Four { get; set; }
+        /// <summary>
+        /// The Brush for the cell containing the Number 5
+        /// </summary>
+        public Brush Five { get; set; }
+        /// <summary>
+        /// The Brush for the cell containing the Number 6
+        /// </summary>
+        public Brush Six { get; set; }
+        /// <summary>
+        /// The Brush for the cell containing the Number 7
+        /// </summary>
+        public Brush Seven { get; set; }
+        /// <summary>
+        /// The Brush for the cell containing the Number 8
+        /// </summary>
+        public Brush Eight { get; set; }
+        /// <summary>
+        /// The Brush for a cell that has been flagged
+        /// </summary>
+        public Brush Flagged { get; set; }
+        /// <summary>
+        /// The Brush for a cell with a mine visible
+        /// </summary>
+        public Brush Mine { get; set; }
+        /// <summary>
+        /// The Brush for a mine that has been detonated
+        /// </summary>
+        public Brush Exploded { get; set; }
+        /// <summary>
+        /// The Brush for a cell with an invalid state
+        /// </summary>
+        public Brush Error { get; set; }
+    }
+
+    /// <summary>
+    /// Container class for various built in themes
+    /// </summary>
+    public static class BrushThemes
+    {
+        /// <summary>
+        /// Assembles a basic VisualBrush with a label for simple brushes with text in it
+        /// </summary>
+        /// <param name="foreground">The Foreground brush</param>
+        /// <param name="background">The Background brush</param>
+        /// <param name="content">The text content</param>
+        /// <returns>A VisualBrush with the given foreground, background, and content</returns>
+        private static VisualBrush GetLabelBrush(Brush foreground, Brush background, string content)
+        {
+            return new VisualBrush() { Visual = new Label() { FontSize = 16, Foreground = foreground, Content = content, Background = background } };
+        }
+
+        /// <summary>
+        /// The default brushes for Minesweeper
+        /// </summary>
+        public static BrushTheme Default { get; } = new BrushTheme()
+        {
+            Hidden = Brushes.Blue,
+            One = GetLabelBrush(Brushes.Black, Brushes.Blue, "1"),
+            Two = GetLabelBrush(Brushes.Black, Brushes.Blue, "2"),
+            Three = GetLabelBrush(Brushes.Black, Brushes.Blue, "3"),
+            Four = GetLabelBrush(Brushes.Black, Brushes.Blue, "4"),
+            Five = GetLabelBrush(Brushes.Black, Brushes.Blue, "5"),
+            Six = GetLabelBrush(Brushes.Black, Brushes.Blue, "6"),
+            Seven = GetLabelBrush(Brushes.Black, Brushes.Blue, "7"),
+            Eight = GetLabelBrush(Brushes.Black, Brushes.Blue, "8"),
+            Flagged = Brushes.Green,
+            Mine = Brushes.Black,
+            Exploded = Brushes.Red,
+            Error = Brushes.HotPink
+        };
+    }
+}
